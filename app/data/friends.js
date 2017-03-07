67d4b23565da0af59700abb0120425ebe03aabd6
mattRCB <mattsteinrcb@gmail.com>
2017-03-07 14:51:49 -0500

    viable product

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,32 @@
+var fs = require('fs');
+
 module.exports = {
 
-	formatSurveyResults: function(body) {
-		var result = {};
-		result.name = body.name;
-		result.photo = body.photo;
-		result.scores = [];
+	findBestMate: function(newUser, callback) {
+		var dbArray;
+		fs.readFile("./app/data/friendsDB.txt", 'utf-8', function(err, data) {
+			if (err) throw err;
+			dbArray = JSON.parse("[" + data + "]");
 
-		for (i = 0; i < 10; i++) {
-			var index = ('question-' + i);
-			result.scores.push(body[index].slice(0, 1));
-		}
-		console.log(result);
-		return result;
-	}
+			var bestMate;
+			var bestSumOfDifs = 41; //starting this at greater than mathematically possible difference.
+			for (var i=0; i<dbArray.length-2; i++) {
+				var crntSumOfDifs = 0;
+				for (var j = 0; j<newUser.scores.length; j++) {
+					crntSumOfDifs += Math.abs(parseInt(newUser.scores[j]) - parseInt(dbArray[i].scores[j]));
+					console.log(dbArray[i].name + ": crntSumOfDifs = " + crntSumOfDifs);
+				}
+				if (crntSumOfDifs < bestSumOfDifs) {
+					bestSumOfDifs = crntSumOfDifs;
+					bestMate = dbArray[i];
+				}			
+			}
+			// console.log("The best programming pal is: " + bestMate.name);
+
+			callback(bestMate);
+
+		});//fs.readFile
+	}//findBestMate
 
 };
 
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -8,6 +8,9 @@ module.exports = function(app) {
 	var bodyParser = require('body-parser');
 
 	app.get("/api/friends", function(req, res) {
+
+		// res.send(getSnapshot());
+
 		var dbArray;
 		fs.readFile("./app/data/friendsDB.txt", 'utf-8', function(err, data) {
 			if (err) throw err;
@@ -15,20 +18,24 @@ module.exports = function(app) {
 			dbArray = "[" + data + "]";
 			console.log(dbArray);
 			res.send(dbArray);
-
 		});
 	});
 
 	app.post("/api/friends", function(req, res) {
-		console.log(JSON.stringify(req.body));
-
+		// console.log(JSON.stringify(req.body));
 		fs.appendFile("./app/data/friendsDB.txt", ("," + JSON.stringify(req.body)), function (err) {
 			if (err) throw err;
 			console.log("Added new user to friendsDB");
 		});
-			
-		// do the logic and res.send() the bestMate data (as json?).
-		res.send(JSON.stringify(req.body));
 
+		friends.findBestMate(req.body, function(bestMate) {
+			console.log(bestMate.name);
+			res.send(JSON.stringify(bestMate));
+			// console.log("apiRoutes logging findBestMate(): " + bestMate + "\n\n");
+
+			// res.send(JSON.stringify(req.body));
+			// do the logic and res.send() the bestMate data (as json?).
+			// res.json(friends.findBestMate(req.body));
+		});
 	}); // app.post
 }; // module.exports
